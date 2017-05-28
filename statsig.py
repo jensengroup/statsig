f0c7628623aaac658ce84f5465c47ac88cd657d6
Jan H. Jensen <jhjensen@sunray>
2017-05-28 13:12:32 +0200

    Added Pearson correlation and print out of Latex table

--- statsig.py
+++ statsig.py
@@ -1,5 +1,22 @@
 import numpy as np
 
+def correl(X,Y):
+    N, = X.shape
+
+    if N < 9:
+        print "not enough points. {} datapoints given. at least 9 is required".format(n)
+        return
+
+    r = np.corrcoef(X,Y)[0][1]
+    r_sig = 1.96/np.sqrt(n-2+1.96**2)
+    F_plus = 0.5*np.log((1+r)/(1-r))+r_sig
+    F_minus = 0.5*np.log((1+r)/(1-r))-r_sig
+    le = r - (np.exp(2*F_minus)-1)/(np.exp(2*F_minus)+1)
+    ue = (np.exp(2*F_plus)-1)/(np.exp(2*F_plus)+1) - r
+
+    return r, le, ue
+
+
 def rmse(X, Y):
     """
     Root-Mean-Square Error
@@ -141,6 +158,10 @@ if __name__ == '__main__':
     me_lower = []
     me_upper = []
 
+    r_list = []
+    r_lower = []
+    r_upper = []
+
     for method in methods:
         mdata = data[method]
 
@@ -161,6 +182,12 @@ if __name__ == '__main__':
         me_list.append(mme)
         me_lower.append(mmee)
         me_upper.append(mmee)
+
+        # r
+        r, rle, rue = correl(mdata, ref)
+        r_list.append(r)
+        r_lower.append(rle)
+        r_upper.append(rue)
 
 
     print "Method_A   Method_B      RMSE_A   RMSE_B   RMSE_A-RMSE_B  Comp Err  same?"
@@ -188,6 +215,52 @@ if __name__ == '__main__':
             significance = abs(rmse_i - rmse_j) < comp_error
 
             print ps.format(m_i, m_j, rmse_i, rmse_j, rmse_i-rmse_j, comp_error, significance)
+
+    print 
+    print "\\begin{table}[]"
+    print "\centering"
+    print "\caption{}"
+    print "\label{}"
+    centering = "\\begin{tabular}{l"
+    for i in range(nm):
+        centering += "c"
+    centering +="}"
+    print centering
+    print "\midrule"
+
+    print "&",
+    for i in range(nm-1):
+        print methods[i],"&",
+    print methods[-1],"\\\\"
+    print "\midrule"
+#   for i in xrange(nm-1):
+#       print '%.1f $\pm$ %.1f/%.1f &'%(rmse_list[i],lower_error[i],upper_error[i]),
+#   print '%.1f $\pm$ %.1f/%.1f'%(rmse_list[-1],lower_error[-1],upper_error[-1])
+    print "RMSE &",
+    for i in xrange(nm-1):
+        print '%.1f &'%(rmse_list[i]),
+    print '%.1f \\\\'%(rmse_list[-1])
+    print "95 \% conf &",
+    for i in xrange(nm-1):
+        print '$\pm$ %.1f/%.1f &'%(upper_error[i],lower_error[i]),
+    print '$\pm$ %.1f/%.1f \\\\'%(upper_error[-1],lower_error[-1])
+    print "ME &",
+    for i in xrange(nm-1):
+        print '%.1f $\pm$ %.1f &'%(me_list[i],me_upper[i]),
+    print '%.1f $\pm$ %.1f \\\\'%(me_list[-1],me_upper[-1])
+    print "$r$ &",
+    for i in xrange(nm-1):
+        print '%.2f &'%(r_list[i]),
+    print '%.2f \\\\'%(r_list[-1])
+    print "95 \% conf &",
+    for i in xrange(nm-1):
+        print '$\pm$ %.2f/%.2f &'%(r_upper[i],r_lower[i]),
+    print '$\pm$ %.2f/%.2f \\\\'%(r_upper[-1],r_lower[-1])
+    print "\midrule"
+    print "\end{tabular}"
+    print "\end{table}"
+    
+
 
 
     # Create x-axis
